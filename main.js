c7aa7909206569a79520326c5af7f301d025a97a
Bagus Irawan <bagusirawansumitro@gmail.com>
2022-02-06 20:11:13 +0700

    add create.html, edit main.js get value, add linethrough and remove function

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+const homeList = document.getElementById("homelist");
+let newList = document.getElementById("listname");
 
+newList.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13 && newList.value != "") {
+    event.preventDefault();
+    document.getElementById("button-create").click();
+    clearTextbox();
+  }
+});
+
+function clearTextbox() {
+  // empty textbox
+  newList.value = "";
+}
+
+function create() {
+  // add new list to ul
+  if (newList.value != "") {
+    let listText = "<li class='home-list px-1'><span onclick='toggle(this)'>" + newList.value + "</span><span onclick='removeList(this)' class='bi-trash trash-ico'></span></li>";
+    homeList.insertAdjacentHTML("afterbegin", listText);
+    clearTextbox();
+  }
+}
+
+function toggle(el) {
+  el.classList.toggle("done");
+}
+
+function removeList(el) {
+  el.parentElement.remove();
+}
