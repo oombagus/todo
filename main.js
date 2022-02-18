db6c7223ec38cc820042aa01a125ddd7c25f6106
Bagus Irawan <bagusirawansumitro@gmail.com>
2022-02-18 16:59:53 +0700

    edit main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const homeList = document.getElementById("homelist");
-let newList = document.getElementById("listname");
+const homeList = document.getElementById("homelist"); // div berisi list
+let newList = document.getElementById("listname"); // textbox
 
 newList.addEventListener("keyup", function (event) {
   if (event.keyCode === 13 && newList.value != "") {
@@ -17,7 +17,10 @@ function clearTextbox() {
 function create() {
   // add new list to ul
   if (newList.value != "") {
-    let listText = `<div class='list-li'><div class='home-list list-text px-1' onclick='toggle(this)'> ${newList.value} </div><div onclick='removeList(this)' class='bi-trash trash-ico list-del'></div></div>`;
+    let listText = `<div class='list-li'>
+      <div class='home-list list-text px-1' onclick='toggle(this)'> ${newList.value} </div>
+      <div onclick='removeList(this)' class='bi-trash trash-ico list-del'></div>
+    </div>`;
     homeList.insertAdjacentHTML("afterbegin", listText);
     clearTextbox();
   }
